ea8158bed58c05a8395e42d0b27a8660033e6867
Julian Peeters <julianpeeters@gmail.com>
2013-08-11 17:02:24 -0700

    created index of entries in order to read arbitrary entries, can now inspect how flags are represented

--- README.md
+++ README.md
@@ -11,4 +11,5 @@ In this example we:
 
 1) Take a simple case class and load the bytes from its Scala sig annotation into a `PickleBuffer`.
 2) Read the bytes back out of the `PickleBuffer` "manually", printing to console.
-3) Read the bytes back out of the `PickleBuffer` using `ShowPickled` from `scala.tools.nsc.util` compiler utils.
+3) Read the bytes back out of the `PickleBuffer` using `ShowPickled` from `scala.tools.nsc.util` compiler utils to interpret the output.
+4) Create an index of entries in order to inspect "deep" entries (needed because only so many entries fit in a pickle buffer at a time, and I don't know how to flush it yet).
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -39,13 +39,25 @@ object Main extends App {
 
 
 
-//Compare with output from `ShowPickled` of `scala.tools.nsc.util` compiler utils
-  val ps = new PrintStream(new File("ShowPickled.output"))
+//To interpret the output, Compare it with output from `ShowPickled` of `scala.tools.nsc.util` compiler utils:
   val duplicateBuffer = new PickleBuffer(bytes, 0, bytes.length)
+  val ps = new PrintStream(new File("ShowPickled.output"))
   ShowPickled.printFile(duplicateBuffer, ps)
 
+/*
+Notice that the numbers that represent the flags are different between our reading and `ShowPickled`.  This means that `ShowPickled` is an imperfect guide for how to write a Scala sig, we'll have to use `readByte`, `readNat`, and `ReadLongNat` directly. 
 
-
-
-
+However problem arises from working with low-level tools: Only so many bytes can be read using the technique above. If one wishes to look further ahead than a few entries (e.g. to see how to represent a flag in a deeper VALsym), one must create an index of the entries and inspect each index position like so:
+*/
+  val triplicateBuffer = new PickleBuffer(bytes, 0, bytes.length)
+  val index: Array[Int] = {
+    val i = triplicateBuffer.createIndex
+    triplicateBuffer.readIndex = 0
+    i
+  }
+  val entryNumber = 129//an arbitrary entry to inspect
+  val entryBuffer = new PickleBuffer(triplicateBuffer.toIndexedSeq(entryNumber)._2, 0, triplicateBuffer.toIndexedSeq(entryNumber)._2.length)
+  entryBuffer.readNat
+  entryBuffer.readNat
+  println("flag: " + entryBuffer.readLongNat)
 }
